537b80078fa785b02ef16f49f839e1dbc885b110
Piotr Praga <piotr.praga@webfleet.com>
2024-07-07 09:57:27 +0200

    platfuse@1.3.4

--- src/lib/scenes/main-scene.ts
+++ src/lib/scenes/main-scene.ts
@@ -6,29 +6,27 @@ import Box from '../models/box'
 
 export default class MainScene extends Scene {
     gravity = 0.02
-    gui?: GUI
+    gui = new GUI()
 
     async init() {
         await super.init()
 
-        // set grid size (rows, cols) and tile size (in pixels)
+        // set grid size (cols, rows) and tile size (in pixels)
         this.setDimensions(vec2(10, 10), vec2(16, 16))
         this.setScale(3)
         this.addLayer(CustomLayer)
 
-        const platfuseBox = this.addObject(new Box(this, { pos: this.size.divide(2) }), 1)
+        const platfuseBox = this.addObject(new Box(this), 1)
         this.camera.toggleBounds(false)
         this.camera.follow(platfuseBox)
 
         console.log('Main Scene initialized', this)
 
         // Dat GUI
-        this.gui = new GUI()
-        this.gui.add(this.game, 'debug').listen()
-
         const f1 = this.gui.addFolder('Scene')
         const f2 = f1.addFolder('Layers')
 
+        this.gui.add(this.game, 'debug').listen()
         f1.add(this, 'gravity').step(0.01).min(0.01).max(1)
         f1.add(this.camera, 'scale').step(0.1).min(1).max(10).listen()
 
